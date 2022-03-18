856e4dd543c0f700c360b917af1ab5de467f9926
Rostislav Kouznetsov <rostislav.kouznetsov@fmi.fi>
2022-03-18 11:55:41 +0200

    Fix for flipped latitude in meteo
    The new workaround for https://code.mpimet.mpg.de/issues/10212
    Meteo re-generated

--- ERA_I-cut/make-infiles.sh
+++ ERA_I-cut/make-infiles.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
 
-##for s in ml sfc.bz2; do s3cmd get s3://Meteo-ERA-Interim-ML/2017/era_interim_2017010112+12.$s ./; done
+if false; then
+  mkdir -p  glob
+  for dd in era_interim_2017010100+06  era_interim_2017010100+12 era_interim_2017010112+06 era_interim_2017010112+12; do
+
+    for s in ml sfc.bz2; do 
+        s3cmd get s3://Meteo-ERA-Interim-ML/2017/$dd.$s ./glob/; 
+    done
+  done
+  s3cmd get s3://Meteo-ERA-Interim-ML/era_interim_1980010100+000.sfc  glob/era_interim_1980010100+0.sfc
+  lbzip2 -d glob/*.bz2
+fi
+
 
 for f in glob/*; do
   outf=`basename $f | sed -e s/era_interim_/era_interim_europe_/`
   cdo  remapnn,grid $f $outf.tmp;
-  grib_set -s editionNumber=1,latitudeOfLastGridPointInDegrees=58.32  $outf.tmp $outf
+  grib_set -s editionNumber=1,jScansPositively=0  $outf.tmp $outf
 done
